1fe6eaf7e7e2bbf890891ea339eb89b1ffe6c0a6
Velorien <k.miczkowski@outlook.com>
2021-02-25 19:53:58 +0100

    Add project files.

---
+++ Controllers/FileController.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileResultBug.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        [HttpGet("getFile1")]
+        public FileResult GetFile1()
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("Hello ASP.NET Core!"));
+            return new FileStreamResult(stream, "text/plain") { FileDownloadName = "hello.txt" };
+        }
+
+        [HttpGet("getFile2")]
+        public ActionResult<FileResult> GetFile2()
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("Hello ASP.NET Core!"));
+            return new FileStreamResult(stream, "text/plain") { FileDownloadName = "hello.txt" };
+        }
+    }
+}
